da5d25f68ff3fed409400dc022819448d70d2bc6
Robinnnnn <rokim8@gmail.com>
2023-03-27 19:22:29 -0400

    iframe

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <iframe
+        title="gallery"
+        src="https://gallery.so/jack"
+        style={{
+          border: 0,
+          width: "100vw",
+          height: "100vh",
+        }}
+      />
     </div>
   );
 }
